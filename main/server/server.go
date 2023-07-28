245f801fa194cc7476e6e204089e59cf81a216bc
Måns Thörnvik <thornvik.mans@gmail.com>
2023-07-28 13:59:46 +0200

    Add global logging option

--- main/server/server.go
+++ main/server/server.go
@@ -6,26 +6,29 @@ import (
 	"net"
 
 	"github.com/maansthoernvik/locksmith/lib/env"
+	"github.com/maansthoernvik/locksmith/lib/log"
+	"go.uber.org/zap"
 )
 
 const ENV_HOST = "LS_HOST"
 const ENV_PORT = "LS_PORT"
 
 func main() {
-	fmt.Println("I'm a server")
+	defer log.Logger.Sync()
+
+	log.Logger.Info("Server listener starting")
 
 	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", env.RequiredString(ENV_HOST), env.RequiredUint16(ENV_PORT)))
 	if err != nil {
-		fmt.Println("Failed to start listener")
-		panic(err)
+		log.Logger.Fatal("Failed to start listener")
 	}
-	fmt.Println("Successfully started listener")
+	log.Logger.Info("Successfully started listener")
 
-	fmt.Println("Awaiting client data...")
+	log.Logger.Info("Awaiting client data...")
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Failed to accept an incoming connection", err)
+			log.Logger.Info("Failed to accept an incoming connection", zap.Error(err))
 			continue
 		}
 
@@ -39,22 +42,22 @@ func handleConnection(conn net.Conn) {
 		buffer := make([]byte, 5)
 		n, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("Error reading from connection:", err.Error())
+			log.Logger.Info("Error reading from connection:", zap.String("error", err.Error()))
 			if err == net.ErrClosed {
-				fmt.Println("The error type indicated that the connection was closed, so terminating this goroutine")
+				log.Logger.Error("The error type indicated that the connection was closed, so terminating this goroutine")
 				break
 			} else if err == io.EOF {
-				fmt.Println("EOF indicated, exiting")
+				log.Logger.Error("EOF indicated, exiting")
 				break
 			}
 			errorCount += 1
 			continue
 		}
-		fmt.Printf("Read %d bytes, message %v, sending reply \n", n, buffer)
+		log.Logger.Info("Read message, sending reply \n", zap.Int("bytes", n), zap.ByteString("message", buffer))
 
 		conn.Write([]byte{1, 1, 1, 1, 1})
 	}
 
-	fmt.Println("Exited connection read loop, terminating...")
+	log.Logger.Info("Exited connection read loop, terminating...")
 	conn.Close()
 }

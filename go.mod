3dd199deaf2e89c91945cd303616a5242ba2b828
Måns Thörnvik <thornvik.mans@gmail.com>
2023-11-10 19:03:33 +0100

    add base ci

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/maansthoernvik/locksmith
 
-go 1.20
+go 1.21
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/maansthoernvik/locksmith/connection"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	connection.Accept()
 }

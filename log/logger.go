7d7dc1bc0267931c91aa19ac00e73f40d50b25b8
Måns Thörnvik <thornvik.mans@gmail.com>
2023-12-04 19:11:42 +0100

    add a rudimentary but good enough logging solution

---
+++ log/logger.go
@@ -0,0 +1,75 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+type LogLevel uint8
+
+const (
+	DEBUG   LogLevel = 0
+	INFO    LogLevel = 1
+	WARNING LogLevel = 2
+	ERROR   LogLevel = 3
+	FATAL   LogLevel = 4
+)
+
+type Logger struct {
+	logLevel LogLevel
+	debug    *log.Logger
+	info     *log.Logger
+	warning  *log.Logger
+	err      *log.Logger
+	fatal    *log.Logger
+}
+
+func New(logLevel LogLevel) *Logger {
+	var logger = &Logger{logLevel: logLevel}
+
+	if logLevel == DEBUG {
+		logger.debug = log.New(os.Stdout, "DEBUG   ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	}
+	if logLevel <= INFO {
+		logger.info = log.New(os.Stdout, "INFO    ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	}
+	if logLevel <= WARNING {
+		logger.warning = log.New(os.Stdout, "WARNING ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	}
+	if logLevel <= ERROR {
+		logger.err = log.New(os.Stdout, "ERROR   ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	}
+	if logLevel <= FATAL {
+		logger.fatal = log.New(os.Stderr, "FATAL   ", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+	}
+
+	return logger
+}
+
+func (logger *Logger) Debug(args ...any) {
+	if logger.logLevel == DEBUG {
+		logger.debug.Println(args...)
+	}
+}
+
+func (logger *Logger) Info(args ...any) {
+	if logger.logLevel <= INFO {
+		logger.info.Println(args...)
+	}
+}
+
+func (logger *Logger) Warning(args ...any) {
+	if logger.logLevel <= WARNING {
+		logger.warning.Println(args...)
+	}
+}
+
+func (logger *Logger) Error(args ...any) {
+	if logger.logLevel <= ERROR {
+		logger.err.Println(args...)
+	}
+}
+
+func (logger *Logger) Fatal(args ...any) {
+	logger.fatal.Fatalln(args...)
+}
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/maansthoernvik/locksmith/connection"
+	"github.com/maansthoernvik/locksmith/log"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-	connection.Accept()
+	logger := log.New(log.DEBUG)
+
+	logger.Debug("this wont show")
+	logger.Info("this INFO shows")
+	logger.Warning("this warning shows")
+	logger.Error("this error shows")
+	logger.Fatal("this fatal error causes a crash")
 }
